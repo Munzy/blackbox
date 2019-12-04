57c60fdbea3b3f33ebe4362bb38bee42ec827af6
Munzy <cm@munroenet.com>
2019-12-03 19:05:54 -0800

    Names!

--- blocks/hostingNames.php
+++ blocks/hostingNames.php
@@ -9,12 +9,14 @@ foreach($list as $item){
     $item = explode(',', $item);
 
     $item[0] = str_replace('"', '', $item[0]);
-    
-    if(!isset(item[2]){
+    if(!isset($item[2])){
     
       $item[2] = null;
       
     }
+
+    $item[2] = str_replace('"', '', $item[2]);
+    $item[2] = str_replace(PHP_EOL, '', $item[2]);
 
     if(is_numeric($item[0])){
 
@@ -22,7 +24,7 @@ foreach($list as $item){
 
             'asn' => $item[0],
             'name' => asName($item[0]),
-            'comment => $item[2]
+            'comment' => $item[2]
     
         );
     
@@ -33,15 +35,16 @@ foreach($list as $item){
 
 ksort($hosting);
 
+echo "ASN, Name" . PHP_EOL;
 foreach($hosting as $host){
   
-  echo $host['asn'] . ',"' . $host['name'] . '","' . $host['comment'] . '"' . PHP_EOL;
+  echo $host['asn'] . ', "' . $host['name'] . '"' . PHP_EOL;
 
 }
 
 function asName($asn){
   
-  $data = json_decode(file_get_contents('https://asn.ipinfo.app/api/json/details/' . $asn), true);
+  $data = json_decode(@file_get_contents('https://asn.ipinfo.app/api/json/details/' . $asn), true);
   
   return $data['name'];
 
